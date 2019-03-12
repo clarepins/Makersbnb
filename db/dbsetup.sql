ac630379600e4aebd580015a4e4493fb1249bebe
Jimjule <roadknightj@gmail.com>
2019-03-12 16:46:49 +0000

    Updated css to display rows, added inline css

--- db/dbsetup.sql
+++ db/dbsetup.sql
@@ -1,6 +1,5 @@
 CREATE DATABASE makersbnb;
-CREATE TABLE listings(id SERIAL PRIMARY KEY, name VARCHAR(500), price INT);
-
+CREATE TABLE listings(id SERIAL PRIMARY KEY, name VARCHAR(500), price INT, description VARCHAR(500), owner_name VARCHAR(100), email VARCHAR(50), phone_num VARCHAR(20));
 
 CREATE DATABASE makersbnb_test;
-CREATE TABLE listings(id SERIAL PRIMARY KEY, name VARCHAR(500), price INT);
+CREATE TABLE listings(id SERIAL PRIMARY KEY, name VARCHAR(500), price INT, description VARCHAR(500), owner_name VARCHAR(100), email VARCHAR(50), phone_num VARCHAR(20));
