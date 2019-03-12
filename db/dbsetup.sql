136c56ca11d0a2a9c5e46714c70bcec25c247be4
matt <mwhitaker1006@gmail.com>
2019-03-12 16:33:04 +0000

    updated db set up docs and readme

--- db/dbsetup.sql
+++
@@ -1,6 +0,0 @@
-CREATE DATABASE makersbnb;
-CREATE TABLE listings(id SERIAL PRIMARY KEY, name VARCHAR(500), price INT);
-
-
-CREATE DATABASE makersbnb_test;
-CREATE TABLE listings(id SERIAL PRIMARY KEY, name VARCHAR(500), price INT);
