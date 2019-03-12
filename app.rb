f4ddc201a2b78b5a44120bc6bcabe5b6f9568df3
Clare Pinder <Clare.pinder@live.co.uk>
2019-03-12 16:04:11 +0000

    Can pull data from add listings page - not testsed yet.

--- app.rb
+++ app.rb
@@ -10,8 +10,15 @@ class Makersbnb < Sinatra::Base
 
  get '/add_listing' do
    erb :add_listing
-end
+ end
 
-
-
+ post '/save_listing' do
+   Listing.add(property_name: params[:property_name_input],
+   price: params[:price_input],
+   description: params[:description_input],
+   owner_name: params[:name_input],
+   email: params[:email_input],
+   phone_num: params[:phone_number_input])
+   redirect '/'
+ end
 end
