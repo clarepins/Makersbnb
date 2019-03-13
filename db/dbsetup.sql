ab88754a05a587ee5a558a8506de88d3ab47d4bd
Jimjule <roadknightj@gmail.com>
2019-03-13 16:44:09 +0000

    Conditional requiring user to be authenticated to see link to add listing page

--- db/dbsetup.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE makersbnb;
-CREATE TABLE listings(id SERIAL PRIMARY KEY, name VARCHAR(500), price INT, description VARCHAR(500), owner_name VARCHAR(100), email VARCHAR(50), phone_num VARCHAR(20));
-<<<<<<< HEAD
-
-=======
->>>>>>> 4a171457a4f8b367ca765c5e8ba0da3c9ba3c567
-
-CREATE DATABASE makersbnb_test;
-CREATE TABLE listings(id SERIAL PRIMARY KEY, name VARCHAR(500), price INT, description VARCHAR(500), owner_name VARCHAR(100), email VARCHAR(50), phone_num VARCHAR(20));
