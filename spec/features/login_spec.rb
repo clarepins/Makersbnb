09b962709864753804c42e1eaf36019bd4a09d7f
matt <mwhitaker1006@gmail.com>
2019-03-14 11:48:16 +0000

    set up simplecov, capybara testing and added first two passing feature tests

---
+++ spec/features/login_spec.rb
@@ -0,0 +1,16 @@
+feature "user signup" do
+  scenario "user can visit homepage and see listings" do
+    visit('/')
+    expect(page).to have_content("Listings post here...")
+  end
+
+  scenario "lets user sign up and go back to homepage" do
+    visit('/')
+    click_button('Sign up')
+    fill_in('email_input', :with => 'test@test.com')
+    fill_in('phone_num_input', :with => '07777777777')
+    fill_in('password_input', :with => 'password')
+    click_button('Submit')
+    expect(page).to have_content("Listings post here...")
+  end
+end
