3b68f12e6e9cf30c1278768866bc33cf093ec32e
Jimjule <roadknightj@gmail.com>
2019-03-15 15:26:35 +0000

    Refactored tests with updated html

--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -1,7 +1,7 @@
 feature "user signup" do
   scenario "user can visit homepage and see listings" do
     visit('/')
-    expect(page).to have_content("Listings post here...")
+    expect(page).to have_content("Listings...")
   end
 
   scenario "lets user sign up and go back to homepage" do
@@ -11,6 +11,6 @@ feature "user signup" do
     fill_in('phone_num_input', :with => '07777777777')
     fill_in('password_input', :with => 'password')
     click_button('Submit')
-    expect(page).to have_content("Listings post here...")
+    expect(page).to have_content("Listings...")
   end
 end
