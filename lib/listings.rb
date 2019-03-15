9dc283d229bcb9b05db798fa5fdd383ec3869d30
matt <mwhitaker1006@gmail.com>
2019-03-15 12:12:51 +0000

    basic fix for single quote problem

--- lib/listings.rb
+++ lib/listings.rb
@@ -21,9 +21,11 @@ class Listing
   end
 
   def self.add(property_name:, price:, description:, user_id:)
-    # These two lines mean SQL query treats a single quote as a string character and doesn't cause a bug
-    property_name = property_name.gsub("'","\\'")
-    description = description.gsub("'","\\'")
+    # These two lines mean SQL query removes single quotes and doesn't break
+    property_name = property_name.gsub("'","")
+    p property_name
+    description = description.gsub("'","")
+    p description
     DatabaseConnection.new.run_query("INSERT INTO listings (id, property_name, price, description, user_id) VALUES(DEFAULT,  '#{property_name}', #{price}, '#{description}', #{user_id});")
   end
 end
